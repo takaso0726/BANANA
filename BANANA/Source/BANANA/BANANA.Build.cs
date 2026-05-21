14109e14d424b56a0de290f3ff896d01dc548674
881PC-\student <nagashima@jec.ac.jp>
2026-05-21 13:40:17 +0900

    no message

---
+++ BANANA/Source/BANANA/BANANA.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class BANANA : ModuleRules
+{
+	public BANANA(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"BANANA",
+			"BANANA/Variant_Horror",
+			"BANANA/Variant_Horror/UI",
+			"BANANA/Variant_Shooter",
+			"BANANA/Variant_Shooter/AI",
+			"BANANA/Variant_Shooter/UI",
+			"BANANA/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ BANANA/Source/BANANA/BANANA.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BANANA.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BANANA, "BANANA" );
+
+DEFINE_LOG_CATEGORY(LogBANANA)

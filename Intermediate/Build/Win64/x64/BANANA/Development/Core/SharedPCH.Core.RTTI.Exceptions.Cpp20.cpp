03c8cd7fe0ddf6d8bc48351b1807d1a515aa765f
takaso0726 <7026sota@gmail.com>
2026-06-27 00:29:28 +0900

    Revert "fuckUE5"
    This reverts commit 77285feff40e201f808ef3ca76aefb0546a68e66.

--- Intermediate/Build/Win64/x64/BANANA/Development/Core/SharedPCH.Core.RTTI.Exceptions.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.51.36248
-#include "SharedPCH.Core.RTTI.Exceptions.Cpp20.h"
--- Intermediate/Build/Win64/x64/BANANA/Development/Engine/SharedPCH.Engine.Project.ValApi.ValExpApi.Cpp20.InclOrderUnreal5_4.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.51.36248
-#include "SharedPCH.Engine.Project.ValApi.ValExpApi.Cpp20.InclOrderUnreal5_4.h"
